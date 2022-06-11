aeaeba2f829cf92e7c3e8834885eaa1d4fab9f73
Doan Viet Hoang <doan.viet.hoang@azoom.jp>
2022-06-11 22:53:00 +0700

    :+1: Refactor get audios

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -11,13 +11,12 @@ router = APIRouter()
 @router.get('/predict')
 async def get_recommend_audios(user_id: int):
   rating_histories = await get_rating_histories()
-  df = pd.DataFrame(item.__dict__ for item in rating_histories).drop(['_sa_instance_state', 'id'], 1)
+  df = pd.DataFrame(item.__dict__ for item in rating_histories).drop(['_sa_instance_state', 'id'], axis=1)
   ratings = df.values
   rs = CF(ratings, k=2, uuCF=1)
   rs.fit()
   recommend_id_list = rs.recommend(user_id)
   recommend_audio_list = []
-  print(recommend_id_list)
   recommend_audio_list = await get_audios_by_ids(recommend_id_list)
   json_compatible_data = jsonable_encoder(recommend_audio_list)
 
